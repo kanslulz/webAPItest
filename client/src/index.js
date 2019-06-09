6964885f099c53c350a11ecd38b3a946e67e77ad
Rayan Kanso <rayankans@chromium.org>
2019-06-09 12:20:38 +0100

    Set up redux Provider and Store

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import ToolBar from './ToolBar.js';
 import Home from './Home.js';
 import featureDescriptors from './features/feature-descriptors.js';
+import store from './redux/store.js'
 
 function IndexRouting() {
   return (
-    <BrowserRouter>
-      <ToolBar />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        {featureDescriptors.map(fd => (<Route exact path={fd.path} component={fd.component} />))}
-        <Redirect from="/*" to="/" />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ToolBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          {featureDescriptors.map(fd => (<Route exact path={fd.path} component={fd.component} />))}
+          <Redirect from="/*" to="/" />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
